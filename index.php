ab7e50a83d220f48e0607ac8555e17fddac35ead
mohitsingh-github <mohit1710158@akgec.ac.in>
2021-06-25 16:40:37 +0530

    email functionality added

--- index.php
+++ index.php
@@ -1,73 +1,32 @@
 <?php
 include('smtp/PHPMailerAutoload.php');
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Marvel Comics</title>
-	<script>if( window.history.replaceState ) {
-  window.history.replaceState( null, null, window.location.href );
-}</script>
-</head>
-<body>
-	<div>
-		<form action="index.php" method="post">
-			<input type="text" name="email" placeholder="email" autocomplete="off" />
-			<input type="submit" name="submit" required />
-		</form>
-	</div>
-<?php
-if($_SERVER['REQUEST_METHOD']==='POST') {
-	$email=$_POST['email'];
-	try{
-		if (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email)) {
-  			echo $email;
-
-
-
-$mail = new PHPMailer;
-
-// Settings
-$mail->IsSMTP();
-$mail->CharSet = 'UTF-8';
-
-$mail->Host       = "smtp.gmail.com";    // SMTP server example
-$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
-$mail->SMTPAuth   = true;                  // enable SMTP authentication
-$mail->Port       = 587;                    // set the SMTP port for the GMAIL server
-$mail->SMTPSecure = 'tls';
-$mail->Username   = "masterrrx007@gmail.com";            // SMTP account username example
-$mail->Password   = "rtcampprojectpass";            // SMTP account password example
-$mail->addAddress($email);
-$mail->addReplyTo("masterrrx007@gmail.com", "Reply");
-// Content
-$mail->isHTML(true);                       
-$mail->Subject = 'Confirmation of your Marvel comic subscription';
-$mail->Body    = 'Thankyou For Subscribing to: <b>Marvel Comics!!</b>';
-$mail->AltBody = 'Thankyou For Subscribing to: Marvel Comics!';
-$mail->SMTPOptions = array(
-                    'ssl' => array(
-                        'verify_peer' => false,
-                        'verify_peer_name' => false,
-                        'allow_self_signed' => true
-                    )
-                );
-try {
-    $mail->send();
-    echo "<br><br>Message has been sent successfully";
-} catch (Exception $e) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-}
-		}
-		else{
-			throw new Exception("<h4>Please enter a valid email address<h4>");
-		}
-	}catch(Exception $err){
-		echo $err->getmessage();
+$html='Msg';
+echo smtp_mailer('chandanvishwas1997@gmail.com','subject',$html);
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->SMTPDebug  = 0;
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->Username = "masterrrx007@gmail.com";
+	$mail->Password = "rtcampprojectpass";
+	$mail->SetFrom("masterrrx007@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
 	}
 }
 ?>
-</body>
-</html>
